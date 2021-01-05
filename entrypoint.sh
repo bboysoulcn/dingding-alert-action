ea0e1c7bad160760d3a9c483eed056e27c9b53ed
bboysoul <bboysoulcn@gmail.com>
2021-01-05 13:03:12 +0800

    change ci

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,23 @@
 
 if [ -z "$DING_SECRET" ]
 then
-  echo "You must provide the action with either a Personal Access Token or the GitHub Token secret in order to deploy."
+  echo "You must provide DING_SECRET"
   exit 1
 fi
 
 if [ -z "$DING_BASE_URL" ]
 then
-  echo "You must provide the action with either a Personal Access Token or the GitHub Token secret in order to deploy."
+  echo "You must provide DING_BASE_URL"
   exit 1
 fi
 if [ -z "$MSG" ]
 then
-  echo "You must provide the action with either a Personal Access Token or the GitHub Token secret in order to deploy."
+  echo "You must provide MSG"
   exit 1
 fi
 if [ -z "$TITLE" ]
 then
-  echo "You must provide the action with either a Personal Access Token or the GitHub Token secret in order to deploy."
+  echo "You must provide TITLE"
   exit 1
 fi
 
